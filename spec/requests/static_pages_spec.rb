2dfb69d0a9d4fb83a05c30b9b9ccbf445ee21de7
Reni-Jerome <reni.jerome@freshworks.com>
2021-01-19 13:44:00 +0530

    added help and home pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "GET /static_pages/home" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')    end
+  end
+end
